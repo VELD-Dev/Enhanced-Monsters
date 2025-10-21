e1b44072c0d046f98d611bd381a236a9d61b2986
Entity378 <150670165+Entity378@users.noreply.github.com>
2025-10-21 18:36:25 +0200

    moved EnsureEnemy2PropPrefabs func
    moved EnsureEnemy2PropPrefabs from GameNetworkManager_Patches to StartOfRound_Patches.

---
+++ Patches/StartOfRound_Patches.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnhancedMonsters.Patches
+{
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class StartOfRound_Patches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("Start")]
+        public static void StartPatch()
+        {
+            var enemies = Resources.FindObjectsOfTypeAll<EnemyAI>();
+
+            foreach (var enemy in enemies)
+            {
+                if (enemy is null)
+                {
+                    Plugin.logger.LogWarning("An enemy is null!");
+                    continue;
+                }
+
+                if (enemy.enemyType is null)
+                {
+                    Plugin.logger.LogWarning($"{enemy.name} has a null enemyType (tf?)");
+                    continue;
+                }
+
+                if (!SyncedConfig.Instance.EnemiesData.ContainsKey(enemy.enemyType.enemyName))
+                {
+                    EnemiesDataManager.RegisterEnemy(enemy.enemyType.enemyName, new());
+                    Plugin.logger.LogInfo($"Mob was not registered. Registered it with name '{enemy.enemyType.enemyName}'");
+                }
+            }
+
+            EnemiesDataManager.SaveEnemiesData();
+            EnemiesDataManager.EnsureEnemy2PropPrefabs();
+        }
+    }
+}
